a35726a68ff538a6a8c9708b569db3f3f354b2d2
Mick <grevmivlos@gmail.com>
2021-03-15 22:54:39 +0100

    Added level 1 and implemented towers

--- Assets/TowerScripts/Tower.cs
+++ Assets/TowerScripts/Tower.cs
@@ -13,12 +13,32 @@ public class Tower : MonoBehaviour
     float lastCheck = 0.0f;
     float checkInterval = 1f;
 
+    public float turnSpeed = 10f;
+
+    public Transform partToRotate;
+    private Transform target = null;
+
+    public float range = 12f;
+
     void Start()
     {
+        GetComponent<SphereCollider>().radius = range;
         audioComp = GetComponent<AudioSource>();
         audioComp.playOnAwake = false;
         audioComp.clip = fireSound;
         audioComp.volume = fireSoundVolume;
+    }
+
+    void Update()
+    {
+        if (target == null)
+            return;
+
+        Vector3 direction = target.transform.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
     }
 
     void OnTriggerStay(Collider col)
@@ -26,10 +46,12 @@ public class Tower : MonoBehaviour
         inTrigger = true;
         if (col.tag == "Enemy" && inTrigger && (Time.time - lastCheck) >= checkInterval)
         {
+            target = col.gameObject.transform;
             Shoot(col);
             lastCheck = Time.time;
         }
     }
+
     void OnTriggerExit(Collider col)
     {
         if (col.tag == "Enemy")
@@ -43,5 +65,10 @@ public class Tower : MonoBehaviour
         GameObject g = Instantiate(cannonballPrefab, transform.position, Quaternion.identity);
         GetComponent<AudioSource>().Play();
         g.GetComponent<Cannonball>().target = col.gameObject.transform.position;
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
